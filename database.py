377ce4fea03008e32c34f27a37684696c6bc547d
Zeke <42339979+hozongsien@users.noreply.github.com>
2021-12-29 17:12:34 +0000

    Included view cart, add to cart, clear cart and checkout features

--- database.py
+++ database.py
@@ -2,17 +2,17 @@
 fruits = {
   'Apple': {
     'name': 'Apple',
-    'price': '0.50',
-    'picture': 'https://unsplash.com/photos/rxN2MRdFJVg'
+    'price': 1.50,
+    'img': 'https://images.unsplash.com/photo-1619546813926-a78fa6372cd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
   },
   'Pear': {
     'name': 'Pear',
-    'price': '0.50',
-    'picture': 'https://images.unsplash.com/photo-1615484477778-ca3b77940c25?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
+    'price': 1.00,
+    'img': 'https://images.unsplash.com/photo-1615484477778-ca3b77940c25?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
   },
   'Orange': {
     'name': 'Orange',
-    'price': '0.50',
-    'picture': 'https://images.unsplash.com/photo-1603664454146-50b9bb1e7afa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+    'price': 2.50,
+    'img': 'https://images.unsplash.com/photo-1603664454146-50b9bb1e7afa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
   },
 }
--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-import json
 import os
 
 import telebot
-from telebot.types import BotCommand, InlineKeyboardButton, InlineKeyboardMarkup
-
+from telebot.types import BotCommand, InlineKeyboardButton, InlineKeyboardMarkup, LabeledPrice
 import database
+
 
 # HackNRollFruitStoreBot
 API_KEY = os.getenv('API_KEY') 
+PAYMENT_KEY = os.getenv('PAYMENT_KEY') 
 bot = telebot.TeleBot(API_KEY)
 
 bot.set_my_commands([
-  BotCommand('parrot', 'Sends back the same message')
+  BotCommand('parrot', 'Sends back the same message'),
+  BotCommand('items', 'Lists all items available in store'),
+  BotCommand('cart', 'Lists all items added in cart'),
+  BotCommand('clear', 'Clears all items in the cart'),
+  BotCommand('checkout', 'Request for payment')
 ])
+
+cart = {}
+
 
 @bot.message_handler(commands=['parrot'])
 def parrot(message):
@@ -20,38 +27,148 @@ def parrot(message):
   print('Received message:', message_text)
   bot.reply_to(message, message_text)
 
-@bot.message_handler(commands=['catalogue'])
-def catalogue(message):
-  print('Received catalogue command')
+
+@bot.message_handler(commands=['items'])
+def items(message):
+  print('Received items command')
   chat_id = message.chat.id
 
   buttons = []
   for fruit_name in database.fruits:
     row = []
-    button = InlineKeyboardButton(fruit_name, 
-      callback_data='catalogue detail ' + fruit_name)
+    button = InlineKeyboardButton(
+      fruit_name, 
+      callback_data='items detail ' + fruit_name
+    )
     row.append(button)
     buttons.append(row)
   
-  bot.send_message(chat_id, 
-    'Catalogue', 
-    reply_markup=InlineKeyboardMarkup(buttons))
+  bot.send_message(
+    chat_id, 
+    'Select the item you would like view in detail', 
+    reply_markup=InlineKeyboardMarkup(buttons)
+  )
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_callback(call):
   chat_id = call.message.chat.id
-  # Find callback data (e.g. 'catalogue Apple detail')
   data = call.data
   intent, data = data.split()[0], data.split()[1:]
   
-  if intent == 'catalogue':
-    send_fruit_details(chat_id, data)
+  if intent == 'items':
+    send_item_details(chat_id, data)
+    return
+  if intent == 'add':
+    add_to_cart(chat_id, data)
     return
 
   bot.send_message(chat_id, 'Callback not implemented')
 
-def send_fruit_details(chat_id, data):
+
+def send_item_details(chat_id, data):
   sub_intent, fruit_name = data
-  bot.send_message(chat_id, 'You tapped on fruit: ' + fruit_name)
+  img_url = database.fruits[fruit_name]['img']
+  price = database.fruits[fruit_name]['price']
+  caption = f'Item: {fruit_name}\nPrice: ${price}\n\nHow many would you like to add to cart?'
+
+  buttons = []
+  count = 0
+  for i in range(1, 4):
+    row = []
+    for j in range(1, 4):
+      count += 1
+      quantity = str(count)
+      button = InlineKeyboardButton(
+        text=quantity,
+        callback_data=f'add {quantity} {fruit_name}'
+      )
+      row.append(button)
+      
+    buttons.append(row)
+
+  bot.send_photo(
+    chat_id, 
+    img_url, 
+    caption=caption,
+    reply_markup=InlineKeyboardMarkup(buttons)
+  )
+
+
+def add_to_cart(chat_id, data):
+  quantity, fruit_name = data
+  if fruit_name not in cart.keys():
+    cart[fruit_name] = int(quantity)
+  else:
+    cart[fruit_name] += int(quantity)
+  
+  bot.send_message(chat_id, f'Added {quantity} {fruit_name}')
+
+
+@bot.message_handler(commands=['cart'])
+def view_cart(message):
+  print('View cart')
+  chat_id = message.chat.id
+  cart_text = 'Cart:\n'
+
+  if cart:
+    for fruit, quantity in cart.items():
+      cart_text += f'- {fruit}: {quantity}\n'
+  else:
+    cart_text = 'Cart is empty!'
+  
+  bot.send_message(
+    chat_id=chat_id,
+    text=cart_text
+  )
+
+
+@bot.message_handler(commands=['clear'])
+def clear_cart(message):
+  print('Clear cart')
+  cart.clear()
+  chat_id = message.chat.id
+  cart_text = 'Cart has been cleared!'
+  
+  bot.send_message(
+    chat_id=chat_id,
+    text=cart_text
+  )
+
+
+@bot.message_handler(commands=['checkout'])
+def checkout_cart(message):
+  print('Checkout cart')
+  chat_id = message.chat.id
+
+  if not cart:
+    bot.send_message(
+      chat_id=chat_id,
+      text='Cart is empty, nothing to checkout!'
+    )
+    return
+  
+  prices = []
+  for fruit_name, quantity in cart.items():
+    item_price = database.fruits[fruit_name]['price']
+    total_item_price_cents = quantity * item_price * 100
+    prices.append(
+      LabeledPrice(
+        label=f'{quantity}x {fruit_name}', 
+        amount=f'{total_item_price_cents:.0f}'
+      )
+    )
+
+
+  bot.send_invoice(
+    chat_id=chat_id,
+    title='Fruit Basket',
+    description='100% Organic. Fresh and sourced locally.',
+    invoice_payload='invoice_payload',
+    provider_token=PAYMENT_KEY,
+    currency='SGD',
+    prices=prices
+  )
+
 
 bot.polling()
