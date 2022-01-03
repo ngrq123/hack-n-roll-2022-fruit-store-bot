18a797930580f810073ab37b8c4671346136db44
Rui Qin Ng <36036788+ngrq123@users.noreply.github.com>
2022-01-03 04:59:20 +0000

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ bot = telebot.TeleBot(API_KEY)
 
 
 def request_start(chat_id):
-  """A helper function to request user to execute command /start"""
+  """
+  Helper function to request user to execute command /start
+  """
   if chat_id not in cart:
     bot.send_message(chat_id=chat_id, text='Please start the bot by sending /start')
   
@@ -23,21 +25,27 @@ def request_start(chat_id):
 
 @bot.message_handler(commands=['parrot'])
 def parrot(message):
-  """The command that replies the user with the text message it recieves."""
+  """
+  Command that replies the user with the text message it receives
+  """
 
   pass
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-  """The command that welcomes the user and configures the required initial setup."""
+  """
+  Command that welcomes the user and configures the initial setup
+  """
 
   pass
 
 
 @bot.message_handler(commands=['items'])
 def items(message):
-  """The commnad that lists all available items for sale."""
+  """
+  Command that lists all available items for sale
+  """
 
   chat_id = message.chat.id
   if chat_id not in cart: 
@@ -49,19 +57,25 @@ def items(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_callback(call):
-  """Handles the execution of the respective functions upon reciept of the callback query."""
+  """
+  Handles the execution of the respective functions upon receipt of the callback query
+  """
 
   pass
 
 
 def view_item_details(chat_id, data):
-  """Displays the item details and an inline keyboard to add the item."""
+  """
+  Displays the item details and an inline keyboard to add the item
+  """
 
   pass
 
 
 def add_to_cart(chat_id, data):
-  """Increments the item's quanity in the cart."""
+  """
+  Increments the item's quantity in the cart
+  """
 
   if chat_id not in cart: 
     request_start(chat_id)
@@ -72,7 +86,9 @@ def add_to_cart(chat_id, data):
 
 @bot.message_handler(commands=['cart'])
 def view_cart(message):
-  """The command that lists all items currently present in cart."""
+  """
+  Command that lists all items currently present in cart
+  """
 
   chat_id = message.chat.id
   if chat_id not in cart: 
@@ -84,7 +100,9 @@ def view_cart(message):
 
 @bot.message_handler(commands=['clear'])
 def clear_cart(message):
-  """The command that removes all previously add items in the cart."""
+  """
+  Command that removes all items in the cart
+  """
 
   chat_id = message.chat.id
   if chat_id not in cart: 
@@ -96,14 +114,18 @@ def clear_cart(message):
 
 @bot.pre_checkout_query_handler(func=lambda query: True)
 def pre_checkout(pre_checkout_query):
-  """Handles the pre checkout payment process event."""
+  """
+  Handles the pre checkout payment process event
+  """
 
   pass
 
 
 @bot.message_handler(commands=['checkout'])
 def checkout_cart(message):
-  """The command that creates an invoice based on the items added to cart."""
+  """
+  Command that creates an invoice based on the items added to cart
+  """
 
   chat_id = message.chat.id
   if chat_id not in cart: 
@@ -115,7 +137,9 @@ def checkout_cart(message):
 
 @bot.message_handler(content_types=['successful_payment'])
 def payment_success(message):
-  """Handles the event upon successful reciept of payments"""
+  """
+  Handles the event upon successful reciept of payment
+  """
   
   pass
 
